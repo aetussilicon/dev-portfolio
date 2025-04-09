7c87635976a864e25ca78fe24d4b80a5724d3c4e
Artur Oliveira <64819062+aetussilicon@users.noreply.github.com>
2025-04-09 10:37:07 -0300

    ✨ feat(app): adicionar componente ProjectsCard para exibir projetos
    - Adiciona o componente ProjectsCard para exibir detalhes do projeto, incluindo uma visualização ao vivo e link do GitHub.
    - Inclui a biblioteca `lucide-react` para ícones.
    - Adiciona um card de exemplo do projeto Listtta na página inicial.
    - Adiciona imagens de placeholder e do projeto Listtta.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './App.css';
 import Apresentation from './components/Apresentation';
+import ProjectsCard from './components/ProjectsCard';
 import Stack from './components/Stack';
 import { motion } from 'framer-motion';
 
 function App() {
     return (
-      <>
-      
-
-        <div className='mx-[15%] p-4 flex flex-col  min-h-screen overflow-hidden'>
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.6, ease: 'easeOut' }}
-            >
-                <div className='mb-20'>
-                    <Apresentation />
+        <>
+            <div className='mx-[15%] p-4 flex flex-col  min-h-screen overflow-hidden'>
+                <motion.div
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, ease: 'easeOut' }}
+                >
+                    <div className='mb-20'>
+                        <Apresentation />
+                    </div>
+                    <Stack />
+                </motion.div>
+                <div className='mt-20'>
+                    <div className='grid grid-cols-2 justify-items-center gap-10'>
+                        <ProjectsCard title='Listtta' description='Encontre tatuadores e piercer na sua região pela Listtta.' stack='Next.js, Java, Spring, Postgresql, React, TypeScript' image={"projects/listtta.png"} liveLink={'https://listtta.com.br'} isOpenSource={false}/>
+                    </div>
                 </div>
-                <Stack />
-            </motion.div>
-        </div>
+            </div>
         </>
     );
 }
---
+++ src/components/ProjectsCard.tsx
@@ -0,0 +1,55 @@
+import { Link } from 'lucide-react';
+import React from 'react';
+
+interface ProjectsCardProps {
+    title: string;
+    description: string;
+    stack: string;
+    image: string;
+    liveLink: string;
+    isOpenSource: boolean;
+    githubLink?: string;
+}
+
+const ProjectsCard: React.FC<ProjectsCardProps> = ({
+    title,
+    description,
+    stack,
+    image,
+    liveLink,
+    isOpenSource = false,
+    githubLink,
+}) => {
+    return (
+        <>
+            <div className='h-120 w-80 bg-white drop-shadow-2xl rounded-xl flex flex-col overflow-hidden'>
+                <div style={{ backgroundImage: `url('${image}')` }} className='h-2/5 bg-center bg-cover rounded-t-xl' />
+
+                <div className='flex flex-col justify-between p-4 h-3/5'>
+                    <div className='flex flex-col gap-2'>
+                        <h1 className='font-semibold text-2xl'>{title}</h1>
+                        <p className='text-justify text-lg text-gray-500'>{description}</p>
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <span className='text-sm text-gray-600'>
+                            <span className='font-extrabold'>Stack:</span> {stack}
+                        </span>
+                        <div className='flex justify-between items-center mt-4'>
+                            <a href={liveLink} target='_blank' className='flex items-center gap-2'>
+                                <Link size={24} /> <span className='underline text-sm'>Live Preview</span>
+                            </a>
+                            {isOpenSource && (
+                                <a href={githubLink} target='_blank' className='flex items-center gap-2'>
+                                    <img src='/icons/github.png' width={24} /> <span className='text-sm'>Código</span>
+                                </a>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProjectsCard;
